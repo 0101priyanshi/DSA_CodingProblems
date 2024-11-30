747adce684d54f17b7c5a054cd468fa44b28f272
Priyanshi Thakur <priyanshi.thakur@Priyanshis-MacBook-Pro.local>
2024-11-30 13:07:23 +0530

    transpose

--- src/main/java/org/example/matrix/Transpose_Matrix.java
+++ src/main/java/org/example/matrix/Transpose_Matrix.java
@@ -1,2 +1,24 @@
-package org.example.matrix;public class Transpose_Matrix {
+/*https://leetcode.com/problems/transpose-matrix/description/*/
+
+package org.example.matrix;
+
+public class Transpose_Matrix {
+    class Solution {
+        public int[][] transpose(int[][] matrix) {
+            int m = matrix.length;
+            int n = matrix[0].length;
+          /*  Since it is not a square matrix so it will throw index out of bound exception for some location
+            in order to prevent that we need to create a copy matrix whose dimension are swapped with original
+            that is row is column count and column count is row count   */
+            int[][] result = new int[n][m];
+            for(int i =0;i<m;i++)
+            {
+                for(int j = 0;j<n;j++)
+                {
+                    result[j][i]=matrix[i][j];
+                }
+            }
+            return result;
+        }
+    }
 }
--- src/main/java/org/example/matrix/Transpose_SquareMatrix.java
+++ src/main/java/org/example/matrix/Transpose_SquareMatrix.java
@@ -1,2 +1,34 @@
-package org.example.matrix;public class Transpose_SqaureMatrix {
+package org.example.matrix;
+
+public class Transpose_SquareMatrix {
+    public static void main(String[] args) {
+
+        int[][] array = new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        int length = array.length;
+
+        /*
+        we need to swap only upper triangular matrix so j
+        is always starting from value of i because if we
+        take j initialized to i then we observe that the
+        matrix is getting back to original
+        */
+        for (int i = 0; i < length; i++) {
+            for (int j = i; j < length; j++) {
+                if (i !=j)
+                {
+                    int temp = array[i][j];
+                    array[i][j] = array[j][i];
+                    array[j][i] = temp;
+                }
+            }
+        }
+        /*print the matrix after swapping*/
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < length; j++)
+            {
+                System.out.print(array[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
 }
